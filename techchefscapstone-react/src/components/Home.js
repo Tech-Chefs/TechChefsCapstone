88cce1f4cb3651d913cc1cb42511f0efe05b7800
TDHouston <trentdhouston@gmail.com>
2022-09-23 15:50:05 -0400

    altering positioning- homepage

--- techchefscapstone-react/src/components/Home.js
+++ techchefscapstone-react/src/components/Home.js
@@ -2,13 +2,16 @@ function Home() {
     return (
         <>
             <body>
-                <section class="home">
-                    <h1>Welcome to TechChefs!</h1>
-                </section>
+                <div className="homebody align-middle">
+                    <section class="home">
+                        <h1>Welcome to TechChefs!</h1>
+                    </section>
 
-                <input type="text" placeholder="Search for Recipes"></input>
-            
-
+                    <form class="d-flex col-6" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
             </body>
         </>
     )
