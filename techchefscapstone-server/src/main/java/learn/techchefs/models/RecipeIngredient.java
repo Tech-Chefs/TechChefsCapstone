31d5bb06c7b9db267ac3c2dc1f42921fb795723b
Evan Maggio <ejmaggio@optonline.net>
2022-09-26 12:12:46 -0400

    added RecipeIngredient model

---
+++ techchefscapstone-server/src/main/java/learn/techchefs/models/RecipeIngredient.java
@@ -0,0 +1,40 @@
+package learn.techchefs.models;
+
+public class RecipeIngredient {
+    Ingredient ingredient;
+    Measurement measurement;
+    boolean isOptional;
+    String preparation;
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public Measurement getMeasurement() {
+        return measurement;
+    }
+
+    public void setMeasurement(Measurement measurement) {
+        this.measurement = measurement;
+    }
+
+    public boolean isOptional() {
+        return isOptional;
+    }
+
+    public void setOptional(boolean optional) {
+        isOptional = optional;
+    }
+
+    public String getPreparation() {
+        return preparation;
+    }
+
+    public void setPreparation(String preparation) {
+        this.preparation = preparation;
+    }
+}
