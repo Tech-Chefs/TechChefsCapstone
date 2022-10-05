cf509e988ee521e5f6c66c9d21b4b7f12327d9bf
Evan Maggio <ejmaggio@optonline.net>
2022-10-05 18:39:58 -0400

    updated sql files

--- techchefscapstone-server/sql/techchefs-pop.sql
+++ techchefscapstone-server/sql/techchefs-pop.sql
@@ -1,14 +1,15 @@
 use meal_curator;
 
 delete from recipe_ingredient_substitute where recipe_id > 0 and primary_ingredient_id > 0 and substitute_ingredient_id > 0;
-delete from recipe_ingredient where recipe_id > 0 and ingredient_id > 0;
-delete from ingredient where id > 0;
 
+delete from recipe_ingredient where recipe_id > 0 and ingredient_id > 0;
+
+delete from ingredient where id > 0;
 alter table ingredient auto_increment = 0;
 
 insert into ingredient (name, contains_dairy, nut_based, meat, fish, animal_based, contains_gluten, contains_egg, contains_soy)
-    select
-        name,
+	select
+		name,
         containts_dairy,
         nut_based,
         meat,
@@ -17,8 +18,8 @@ insert into ingredient (name, contains_dairy, nut_based, meat, fish, animal_base
         contains_gluten,
         contains_egg,
         contains_soy
-    from ingredients_data;
-    
+	from ingredients_data;
+
 update ingredient i1
 inner join ingredients_data d
 on i1.name = d.name
@@ -35,56 +36,56 @@ delete from recipe where id > 0;
 alter table recipe auto_increment = 0;
 
 insert into recipe (user_id, name, description)
-    select
-        u.app_user_id,
+	select
+		u.app_user_id,
         d.name,
         d.description
-    from recipes_data d
+	from recipes_data d
     inner join app_user u
     on d.username = u.username;
-    
+
 select * from recipe;
 
 insert into recipe_ingredient (recipe_id, ingredient_id, unit_id, quantity, optional, preparation)
-    select
-        r.id,
+	select
+		r.id,
         i.id,
         u.id,
         d.quantity,
         d.optional,
         d.preparation
-    from recipe_ingredients_data d
+	from recipe_ingredients_data d
     inner join recipe r
     on d.recipe_name = r.name
     inner join ingredient i
     on d.ingredient_name = i.name
     inner join unit u
     on d.unit_short_name = u.short_name;
-    
+
 select * from recipe_ingredient;
 
 insert into recipe_ingredient_substitute (recipe_id, primary_ingredient_id, substitute_ingredient_id)
-    select
-        r.id,
+	select
+		r.id,
         i1.id,
         i2.id
-    from substitutions_data d
+	from substitutions_data d
     inner join recipe r
     on d.recipe_name = r.name
     inner join ingredient i1
     on d.primary_ingredient_name = i1.name
     inner join ingredient i2
     on d.substitution_ingredient_name = i2.name;
-    
+
 select * from recipe_ingredient_substitute;
 
 insert into recipe_direction (recipe_id, step_num, instruction)
-    select
-        r.id,
-        d.step_num,
-        d.instruction
-    from directions_data d
+	select
+		r.id,
+		d.step_num,
+		d.instruction
+	from directions_data d
     inner join recipe r
     on d.recipe_name = r.name;
-    
+
 select * from recipe_direction;
--- techchefscapstone-server/sql/techchefs-prod.sql
+++ techchefscapstone-server/sql/techchefs-prod.sql
@@ -1,43 +1,77 @@
-drop database if exists meal_curator;
-create database meal_curator;
+/*drop database if exists meal_curator;
+create database meal_curator;*/
 
 use meal_curator;
 
+DROP TABLE IF EXISTS `list_item` ;
+DROP TABLE IF EXISTS `shopping_list` ;
+drop table if exists recipe_ingredient_substitute;
+drop table if exists recipe_ingredient;
+drop table if exists recipe_direction;
+drop table if exists recipe;
+ drop table if exists ingredient_unit_unit;
+drop table if exists ingredient;
+drop table if exists unit_unit;
+drop table if exists unit;
+drop table if exists app_user_role;
 drop table if exists app_user;
+drop table if exists app_role;
+
 create table app_user (
-	id int primary key auto_increment,
+    app_user_id int primary key auto_increment,
     username varchar(50) not null unique,
-    password_hash varchar(2048) not null
+    password_hash varchar(2048) not null,
+    disabled bit not null default(0)
 );
 
-drop table if exists ingredient;
+select * from recipe_direction where recipe_id = 20;
+
+create table app_role (
+    app_role_id int primary key auto_increment,
+    `name` varchar(50) not null unique
+);
+
+create table app_user_role (
+    app_user_id int not null,
+    app_role_id int not null,
+    constraint pk_app_user_role
+        primary key (app_user_id, app_role_id),
+    constraint fk_app_user_role_user_id
+        foreign key (app_user_id)
+        references app_user(app_user_id),
+    constraint fk_app_user_role_role_id
+        foreign key (app_role_id)
+        references app_role(app_role_id)
+);
+
 create table ingredient (
 	id int primary key auto_increment,
     name varchar(50) not null,
     user_id int,
     parent_id int,
-    contains_dairy tinyint(1) not null,
-    nut_based tinyint(1) not null,
-    meat tinyint(1) not null,
-    fish tinyint(1) not null,
-    animal_based tinyint(1) not null,
-    contains_gluten tinyint(1) not null,
-    kosher tinyint(1) not null,
-    contains_egg tinyint(1) not null,
-    contains_soy tinyint(1) not null,
-    constraint fk_user_id
+    contains_dairy bit not null,
+    nut_based bit not null,
+    meat bit not null,
+    fish bit not null,
+    animal_based bit not null,
+    contains_gluten bit not null,
+    contains_egg bit not null,
+    contains_soy bit not null,
+    constraint fk_ingredient_user_id
 		foreign key (user_id)
-        references app_user(id)
+        references app_user (app_user_id)
 );
 
-drop table if exists recipe;
 create table recipe (
 	id int primary key auto_increment,
+    user_id int not null,
     name varchar(50) not null,
-    description varchar(1000) not null
+    description varchar(2000) not null,
+    constraint fk_recipe_user_id
+		foreign key (user_id)
+        references app_user (app_user_id)
 );
 
-drop table if exists unit_unit;
 drop table if exists unit;
 create table unit (
 	id int primary key auto_increment,
@@ -45,11 +79,10 @@ create table unit (
     long_name varchar(50) not null
 );
 
-drop table if exists unit_unit;
 create table unit_unit (
 	unit_1_id int,
     unit_2_id int,
-    factor decimal(5) not null,
+    factor decimal(7,2) not null,
     constraint fk_unit_1_id
 		foreign key (unit_1_id)
         references unit(id),
@@ -58,74 +91,89 @@ create table unit_unit (
         references unit(id)
 );
 
-drop table if exists ingredient_unit_unit;
 create table ingredient_unit_unit (
 	ingredient_id int,
     unit_1_id int,
     unit_2_id int,
-    factor decimal(5) not null,
-    constraint fk_ingredient_id
+    factor decimal(7,2) not null,
+    constraint fk_unit_ingredient_id
 		foreign key (ingredient_id)
         references ingredient(id),
-    constraint fk_unit_1_id_foringredient
+    constraint fk_ingredient_unit_1_id
 		foreign key (unit_1_id)
         references unit(id),
-	constraint fk_unit_2_id_foringredient
+	constraint fk_ingredient_unit_2_id
 		foreign key (unit_2_id)
         references unit(id)
 );
 
-drop table if exists recipe_ingredient;
 CREATE TABLE recipe_ingredient (
   `recipe_id` INT NOT NULL,
   `ingredient_id` INT NOT NULL,
-  `quantity` DECIMAL(5) NOT NULL,
+  `quantity` DECIMAL(7, 4) NOT NULL,
   `unit_id` INT NOT NULL,
-  `optional` TINYINT(1) NOT NULL,
+  `optional` bit NOT NULL,
   `preparation` VARCHAR(45),
   PRIMARY KEY (`recipe_id`, `ingredient_id`),
-  CONSTRAINT `fk_recipe_id`
+  CONSTRAINT `fk_ingredient_recipe_id`
     FOREIGN KEY (`recipe_id`)
-    REFERENCES recipe (`id`)
+    REFERENCES `recipe` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_ingredient_id_recipeIngredient`
+  CONSTRAINT `fk_recipe_ingredient_id`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES ingredient (`id`),
-  CONSTRAINT `fk_unit_id_recipeIngredient`
+  CONSTRAINT `fk_unit_id`
     FOREIGN KEY (`unit_id`)
     REFERENCES unit (`id`)
 );
 
-DROP TABLE IF EXISTS `shopping_list` ;
+create table recipe_ingredient_substitute (
+	recipe_id int not null,
+    primary_ingredient_id int not null,
+    substitute_ingredient_id int not null,
+    primary key (recipe_id, primary_ingredient_id, substitute_ingredient_id),
+    foreign key (recipe_id) references recipe (id),
+    foreign key (primary_ingredient_id) references ingredient (id),
+    foreign key (substitute_ingredient_id) references ingredient (id)
+);
+
+create table recipe_direction (
+	recipe_id int not null,
+    step_num int not null,
+    instruction varchar(500),
+    primary key (recipe_id, step_num),
+    foreign key (recipe_id) references recipe (id)
+);
+
 CREATE TABLE `shopping_list` (
   `id` INT primary key,
   `date_added` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
-  CONSTRAINT `fk_user_id_shoppingList`
+  CONSTRAINT `fk_list_user_id`
     FOREIGN KEY (`user_id`)
-    REFERENCES `app_user` (`id`)
+    REFERENCES `app_user` (`app_user_id`)
 );
 
-DROP TABLE IF EXISTS `list_item` ;
 CREATE TABLE `list_item` (
   `list_id` INT NOT NULL,
   `ingredient_id` INT NOT NULL,
   `quantity` DECIMAL(5) NOT NULL,
   `unit_id` INT NOT NULL,
   PRIMARY KEY (`list_id`, `ingredient_id`),
-  CONSTRAINT `fk_list_id_listItem`
+  CONSTRAINT `fk_list_id`
     FOREIGN KEY (`list_id`)
     REFERENCES `shopping_list` (`id`),
-  CONSTRAINT `fk_ingredient_id_listItem`
+  CONSTRAINT `fk_list_ingredient_id`
     FOREIGN KEY (`ingredient_id`)
     REFERENCES `ingredient` (`id`),
-  CONSTRAINT `fk_unit_id_listItem`
+  CONSTRAINT `fk_list_unit_id`
     FOREIGN KEY (`unit_id`)
     REFERENCES `unit` (`id`)
 );
 
 insert into unit (short_name, long_name) values
+	('whole', 'whole'),
 	('tsp', 'teaspoon'),
     ('tbsp', 'tablespoon'),
     ('floz', 'fluid ounce'),
@@ -163,3 +211,21 @@ insert into unit_unit (unit_1_id, unit_2_id, factor) values
     (select id from unit where short_name = 'g'), 0.35),
     ((select id from unit where short_name = 'lb'),
     (select id from unit where short_name = 'kg'), 2.2);
+
+-- passwords are set to "P@ssw0rd!"    
+insert into app_user (username, password_hash, disabled)
+	values
+    ('marySue82', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0),
+    ('johnDoe64', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0),
+    ('anneMann99', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0),
+    ('admin1', '$2a$10$ntB7CsRKQzuLoKY3rfoAQen5nNyiC/U60wBsWnnYrtQQi8Z3IZzQa', 0);
+    
+insert into app_role (`name`) values
+    ('USER'),
+    ('ADMIN');
+    
+insert into app_user_role (app_user_id, app_role_id) values
+	(1, 1),
+    (2, 1),
+    (3, 1),
+    (4, 2);
